535892faccfa37e270304b69cc4e2f697243a209
Gábor Görzsöny <gabor@gorzsony.com>
2018-08-09 22:45:21 +0200

    Added copyright

--- src/behaviors/ContainerBehavior.h
+++ src/behaviors/ContainerBehavior.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) Gábor Görzsöny <gabor@gorzsony.com> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
 #ifndef CONTAINERBEHAVIOR_H_INCLUDED
 #define CONTAINERBEHAVIOR_H_INCLUDED
 
